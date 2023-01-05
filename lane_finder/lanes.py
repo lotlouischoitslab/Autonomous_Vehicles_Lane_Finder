349b7108aa20114159aa32a3abaec44964812773
lotlouischoitslab <louis.s.cho@gmail.com>
2023-01-04 20:41:05 -0600

    add

--- lane_finder/lanes.py
+++ lane_finder/lanes.py
@@ -2,13 +2,39 @@ import cv2
 import numpy as np 
 import matplotlib.pyplot as plt
 
+# Make coordinates
+def make_coordinates(image,line_parameters):
+    slope,intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1,y1,x2,y2])
+
 # Average slope intercept
 def average_slope_intercept(image,lines):
     left_fit = []
     right_fit = []
     for line in lines:
-        x1,y1,x2,y2 = lines.reshape(4)
+        x1,y1,x2,y2 = line.reshape(4)
+        parameters = np.polyfit((x1,x2),(y1,y2),1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope,intercept))
+        else:
+            right_fit.append((slope,intercept))
     
+    # print(left_fit)
+    # print(right_fit)
+
+    left_fit_average = np.average(left_fit,axis=0)
+    right_fit_average = np.average(right_fit,axis=0)
+    
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+
+    return np.array([left_line,right_line])
 
 # Canny function traces the gradient of the roads
 def canny(lane_image):
@@ -45,13 +71,30 @@ def region_of_interest(image):
     return masked_image
 
 if __name__ == '__main__':
-    image = cv2.imread('test_image.jpg')
-    lane_image = np.copy(image)
-    canny_image = canny(lane_image)
-    cropped_image = region_of_interest(canny_image)
-    lines = cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
-    line_image = display_lines(lane_image,lines)
-    combo_image = cv2.addWeighted(lane_image,0.8,line_image,1,1)
-    cv2.imshow('result',combo_image)
-    cv2.waitKey(0)
+    # image = cv2.imread('test_image.jpg')
+    # lane_image = np.copy(image)
+    # canny_image = canny(lane_image)
+    # cropped_image = region_of_interest(canny_image)
+    # lines = cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
+    # averaged_lines = average_slope_intercept(lane_image, lines)
+    # line_image = display_lines(lane_image,averaged_lines)
+    # combo_image = cv2.addWeighted(lane_image,0.8,line_image,1,1)
+    # cv2.imshow('result',combo_image)
+    # cv2.waitKey(0)
 
+    cap = cv2.VideoCapture('test2.mp4')
+    while(cap.isOpened()):
+        _, frame = cap.read()
+        canny_image = canny(frame)
+        cropped_image = region_of_interest(canny_image)
+        lines = cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
+        averaged_lines = average_slope_intercept(frame, lines)
+        line_image = display_lines(frame,averaged_lines)
+        combo_image = cv2.addWeighted(frame,0.8,line_image,1,1)
+        cv2.imshow('result',combo_image)
+
+        # Numeric keyboard
+        if cv2.waitKey(3) and 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
